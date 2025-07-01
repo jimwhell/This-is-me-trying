f135bb7cd9100788a9da619bdba6f7d59b9e786e
Jimwell Manalo <j.manalo@coi.local>
2025-07-01 15:03:56 +0800

    (chore): Create comments and likes methods

--- server/app/Models/Post.php
+++ server/app/Models/Post.php
@@ -9,4 +9,12 @@ class Post extends Model
     protected $fillable= [
         'content', 'user_id', 'song_id'
     ];
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
 }
