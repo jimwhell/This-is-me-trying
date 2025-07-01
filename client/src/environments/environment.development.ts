8283538c8745941a2b2029afaa013e72447014d6
Jimwell Manalo <j.manalo@coi.local>
2025-07-01 15:24:28 +0800

    (chore): Delete angular project

--- client/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api', // Adjust the URL as needed
-  notesUrl: 'http://localhost:8000/api/notes', // Adjust the URL as needed
-};
